c09f08f678e895a36bac61303d594321cf7eee6a
RaymondLoranger <Raymond.Loranger@bell.net>
2018-03-26 20:20:57 -0400

    0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PersistConfig.Mixfile do
   def project do
     [
       app: :persist_config,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       name: "Persist Config",
@@ -44,7 +44,8 @@ defmodule PersistConfig.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_tasks, path: "../mix_tasks", only: :dev, runtime: false},
+      {:mix_tasks,
+       github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false}
