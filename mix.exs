822ea0afd2b7aa1dc04be42e3fd18844dd5cdc08
RaymondLoranger <Raymond.Loranger@bell.net>
2026-04-27 20:17:34 -0400

    "0.4.48"

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PersistConfig.MixProject do
   def project do
     [
       app: :persist_config,
-      version: "0.4.47",
+      version: "0.4.48",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Persist Config",
